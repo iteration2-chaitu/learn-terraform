145fe480fe027c262e008625a649ed05f99f5ba7
chaitu <chaitus.ch09@gmail.com>
2024-10-07 11:29:40 -0700

    create a pipelines for terraform

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -28,25 +28,25 @@ data "aws_security_group" "selected" {
 # 1. If provisioner fails then instance we dont want to re-create my instance.
 # 2. Requirements emerge in a way that we need to re-run the provisioner again and again  (meaning those provisioning commands we need to run again and again)
 
-#resource "aws_instance" "test1" {
-#  ami                    = "ami-09c813fb71547fc4f"
-#  instance_type          = "t3.small"
-#  vpc_security_group_ids = [data.aws_security_group.selected.id]
-#}
-#
-#resource "null_resource" "provisioner" {
-#  provisioner "remote-exec" {
-#
-#    connection {
-#      type     = "ssh"
-#      user     = "ec2-user"
-#      password = "DevOps321"
-#      host     = aws_instance.test1.public_ip
-#    }
-#
-#    inline = [
-#      "dnf install nginx -y",
-#      "sudo systemctl start nginx"
-#    ]
-#  }
-#}
+resource "aws_instance" "test1" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test1.public_ip
+    }
+
+    inline = [
+      "dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
